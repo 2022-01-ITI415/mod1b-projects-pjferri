145323f06bcd9e0972dae85d98550a2749b05c24
Peter Ferri <pjferri10@gmail.com>
2022-02-16 03:34:17 -0500

    Created a Game Over Setup when you lose

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     public Text scoreText;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         scoreText.text = "Enemies Defeated: " + Enemy.score.ToString();
     }
+
+    public void RetryButton() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
